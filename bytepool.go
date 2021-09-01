f1901783e9d1ca08c419e6bd368fde032bd453d8
ma6174 <ma617495@gmail.com>
2021-09-01 20:33:14 +0800

    pointer -> idx

--- bytepool.go
+++ bytepool.go
@@ -4,14 +4,14 @@ import (
 	"context"
 	"runtime"
 	"sync"
+	"sync/atomic"
 	"time"
-	"unsafe"
 )
 
 // NewBytePool 生成的pool会限制最多分配 maxCount*bufferSize 大小内存，
 // 超过之后会阻止继续分配内存防止程序oom
 func NewBytePool(maxCount, bufferSize int, gcDuration time.Duration) *BytePool {
-	ch := make(chan []byte, maxCount)
+	ch := make(chan *Bs, maxCount)
 	for i := 0; i < maxCount; i++ {
 		ch <- nil
 	}
@@ -22,52 +22,72 @@ func NewBytePool(maxCount, bufferSize int, gcDuration time.Duration) *BytePool {
 	return bp
 }
 
+var idx uint64
+
+func getNextIdx() uint64 {
+	return atomic.AddUint64(&idx, 1)
+}
+
+type Bs struct {
+	idx uint64
+	bs  []byte
+	bp  *BytePool
+}
+
+func (d *Bs) Data() []byte {
+	return d.bs
+}
+
+func (d *Bs) PutBack() {
+	d.bp.put(d)
+}
+
 type BytePool struct {
-	ch   chan []byte
+	ch   chan *Bs
 	size int
 	all  sync.Map
 }
 
 // Get 在全部分配完的情况下会一直等待直到拿到数据
-func (bp *BytePool) Get() []byte {
-	buf, _ := bp.GetWithCtx(context.Background())
-	return buf
+func (bp *BytePool) Get() *Bs {
+	bs, _ := bp.GetWithCtx(context.Background())
+	return bs
 }
 
 // GetWithCtx 可以通过context的超时来控制不再等待提前返回
-func (bp *BytePool) GetWithCtx(ctx context.Context) (b []byte, err error) {
+func (bp *BytePool) GetWithCtx(ctx context.Context) (bs *Bs, err error) {
 	select {
 	case <-ctx.Done():
 		err = ctx.Err()
-	case b = <-bp.ch:
-		if b == nil {
-			b = make([]byte, bp.size)
+	case bs = <-bp.ch:
+		if bs == nil {
+			bs = &Bs{
+				idx: getNextIdx(),
+				bs:  make([]byte, bp.size),
+				bp:  bp,
+			}
 			// 如果用户忘记把数据put回来，这里put一个nil进去，
 			// 保证channel里面的元素个数不减少
-			runtime.SetFinalizer(&b, func(b *[]byte) {
-				bp.Put(nil)
+			runtime.SetFinalizer(bs, func(bs *Bs) {
+				bs.PutBack()
 			})
 		} else {
-			bp.all.Delete(uintptr(unsafe.Pointer(&b)))
+			bp.all.Delete(bs.idx)
 		}
 	}
 	return
 }
 
-func (bp *BytePool) Put(p []byte) {
-	if cap(p) != bp.size {
-		p = nil
-	} else {
-		p = p[:bp.size]
+func (bp *BytePool) put(bs *Bs) {
+	if bs != nil {
 		// 检测p是否重复放回，防止重复分配导致数据错乱
-		_, loaded := bp.all.LoadOrStore(
-			uintptr(unsafe.Pointer(&p)), nil)
+		_, loaded := bp.all.LoadOrStore(bs.idx, nil)
 		if loaded {
 			return
 		}
 	}
 	select {
-	case bp.ch <- p:
+	case bp.ch <- bs:
 	default:
 	}
 }
@@ -81,13 +101,14 @@ func (bp *BytePool) loopGC(gcDuration time.Duration) {
 		// 强制执行一次GC，让 runtime.SetFinalizer 里面的函数执行
 		runtime.GC()
 		select {
-		case b := <-bp.ch:
-			if b != nil {
+		case bs := <-bp.ch:
+			if bs != nil {
+				bs.bs = nil
 				// 主动清理的元素清除原来设置的runtime.SetFinalizer，
 				// 防止重复放回
-				runtime.SetFinalizer(&b, nil)
+				runtime.SetFinalizer(bs, nil)
 			}
-			bp.Put(nil)
+			bp.put(nil)
 		default:
 		}
 	}
--- bytepool_test.go
+++ bytepool_test.go
@@ -13,7 +13,8 @@ func TestBytePool(t *testing.T) {
 	p := NewBytePool(2, 3, 0)
 	require.Equal(t, 2, len(p.ch))
 	require.Equal(t, 3, p.BufferSize())
-	b := p.Get()
+	bs := p.Get()
+	b := bs.Data()
 	require.Equal(t, []byte{0, 0, 0}, b)
 	require.Equal(t, 1, len(p.ch))
 	p.Get()
@@ -24,31 +25,35 @@ func TestBytePool(t *testing.T) {
 	require.Equal(t, context.DeadlineExceeded, err)
 	// change data and get the same data
 	b[0] = 1
-	p.Put(b)
+	bs.PutBack()
 	require.Equal(t, 1, len(p.ch))
 	// put same buffer no effect
-	p.Put(b)
+	bs.PutBack()
 	require.Equal(t, 1, len(p.ch))
 	b2 := p.Get()
-	require.Equal(t, []byte{1, 0, 0}, b2)
-	b = nil
+	require.Equal(t, []byte{1, 0, 0}, b2.Data())
+	bs = nil
 	// forgot to reput, gc
 	runtime.GC()
+	runtime.GC()
 	require.Equal(t, 2, len(p.ch))
-	p.Put(nil)
+	p.put(nil)
 	require.Equal(t, 2, len(p.ch))
 }
 
 func TestGC(t *testing.T) {
 	p := NewBytePool(1, 3, 100*time.Millisecond)
-	b := p.Get()
+	bs := p.Get()
+	b := bs.Data()
 	b[0] = 1
-	p.Put(b)
-	b = p.Get()
+	bs.PutBack()
+	bs = p.Get()
+	b = bs.Data()
 	require.Equal(t, []byte{1, 0, 0}, b)
-	p.Put(b)
+	bs.PutBack()
 	b = nil
 	time.Sleep(150 * time.Millisecond)
-	b = p.Get()
+	bs = p.Get()
+	b = bs.Data()
 	require.Equal(t, []byte{0, 0, 0}, b)
 }
